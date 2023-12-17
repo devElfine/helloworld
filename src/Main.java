ddd0d3c1c6a2763b8876fa2f0cac205a38222049
devElfine <andrejkosygin@yandex.ru>
2023-12-18 03:39:14 +1000

    hw7 hang

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,15 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello world and everybody!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        // task 9
+        System.out.println("\nTask 9 result:");
+        float advanced = 12;
+        float totalDep = 0f;
+        for (int i = 1; i <= 12; i++) {
+            totalDep = totalDep + (advanced * 1.01f);
+            System.out.printf("Месяц " + monthsVerb[i - 1] + ". Сумма накоплений составляет " + totalDep +
+                    " рублей.\n");
         }
     }
 }
