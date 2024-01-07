bfa92f597af08068d703ecbbec8079ad653ec95f
devElfine <andrejkosygin@yandex.ru>
2024-01-07 22:33:51 +1000

    hw10 ready

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,26 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("--hw10-strings--");
-        System.out.println("\nTask 1 result:");
 
-        }
+        // Task 1
+        System.out.println("\nTask 1 result:");
+        String lastName = "Ivanov";
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+
+        System.out.println("Employee`s L.F.M \u2014 " + lastName + " " + firstName + " " + middleName + ".");
+
+        // Task 2
+        System.out.println("\nTask 2 result:");
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Employee`s L.F.M for filling out the report \u2014 " + fullName.toUpperCase() + ".");
+
+        // Task 3
+        System.out.println("\nTask 3 result:");
+        String lastNameA = "Иванов";
+        String firstNameA = "Семён";
+        String middleNameA = "Семёнович";
+        String fullNameA = lastNameA + " " + firstNameA + " " + middleNameA;
+        System.out.println("Данные Ф.И.О. сотрудника \u2014 " + fullNameA.replace('ё','е') + ".");
     }
 }
