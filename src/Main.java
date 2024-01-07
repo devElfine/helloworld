1b2c27e212d385f7880407a1da94a0238ae3ef41
devElfine <andrejkosygin@yandex.ru>
2024-01-07 22:02:46 +1000

    hw10 hang

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("--hw10-strings--");
+        System.out.println("\nTask 1 result:");
 
         }
     }
