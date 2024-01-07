cf16abe3d8a869346b93be7b070fbfdd494e4593
devElfine <andrejkosygin@yandex.ru>
2024-01-07 23:05:06 +1000

    hw11-methods hang

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,22 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static int isLeap(int aYear) {
+        int ret = 0;
+        if (aYear > 1584) {
+            if (((aYear % 400 == 0) || (aYear % 4 == 0)) && (aYear % 100 != 0)) {
+                ret = 1;
+            } else {
+                ret = -1;
+            }
+        }
+        return ret;
+    }
     public static void main(String[] args) {
         System.out.println("--hw11-methods--");
 
         // task 1
         System.out.println("\nTask 1 result:");
-        public static void isLeap(int aYear) {
-            if (aYear > 1584) {
-                if (((aYear % 400 == 0) || (aYear % 4 == 0)) && (aYear % 100 != 0)) {
-                    return 1;
-                } else {
-                    return -1;
-                }
-            }
-        }
+        System.out.println(isLeap(2021));
     }
 }
