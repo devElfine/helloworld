78ee67d26175720b9f6c994a43767f78000a1dee
devElfine <andrejkosygin@yandex.ru>
2024-01-08 05:44:19 +1000

    hw12-objects-and-classes hang

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        //--hw#-CAP--
-        //Task #
+        //--hw12-objects-and-classes--
+        //Task 1
         }
     }
 }
