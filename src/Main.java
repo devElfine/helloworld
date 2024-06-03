8fd78429de17076ce8ee4bceaa4366614aa65e8d
devElfine <andrejkosygin@yandex.ru>
2024-06-04 02:00:26 +1000

    final_1 almost there, but little unfinished

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,22 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    private static Employee employee[] = new Employee[10];
-    private static void fillStaff() { /* Заполнение списка сотрудников, кроме первого и последнего, итого 8 человек */
-        int k = employee.length;
-        for (int i = 1; i < 6; i++) {
-            employee[i] = new Employee("n" + i,"s" + i,"p" + i, i,i * 1000);
-        }
-    }
-    private static String echoStaff() { /* Извлечение полного списка сотрудников */
-        String output = "";
-        int k = employee.length;
-        for (int i = 0; i < k; i++) {
-            if (employee[i] != null)
-                output = output + employee[i].toString();
-        }
-        return output;
-//        return employee[1].toString().concat(employee[2].toString());
-    }
+    public static EmployeeBook EmployeeBook = new EmployeeBook();
     public static void main(String[] args) {
-//        final_1
-//        System.out.println(employee[0].toString());
-        fillStaff();
-        System.out.println("\tПолный список сотрудников:");
-        System.out.println(echoStaff());
+        System.out.println(EmployeeBook.Staff(true));
+        System.out.println(EmployeeBook.TotalSalary());
+        System.out.println(EmployeeBook.MinSalary());
+        System.out.println(EmployeeBook.MaxSalary());
+        System.out.println(EmployeeBook.MiddleSalary());
+        System.out.println("Добавлен новый служащий, id [" + EmployeeBook.addStaff("Семён", "Павлович", "Батькович", 3,
+            33555.50d) + "].");
+        System.out.println(EmployeeBook.Staff(true));
+        System.out.println(EmployeeBook.MiddleSalary());
+//        System.out.println(EmployeeBook.CorrectSalary(10));
+//        System.out.println(EmployeeBook.Staff(false));
+//        System.out.println(EmployeeBook.Staff(false));
     }
 }
